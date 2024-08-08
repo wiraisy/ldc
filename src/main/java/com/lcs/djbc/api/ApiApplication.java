20392c292fc61790e8bd144bb2a695d92a575ab7
Akbar Wiraisy <https://dc1bitbuckapp12/projects/BIP/repos/java-backend/browse?at=refs%2Fheads%2Frelease%2F31%2F1%2F2023>
2024-08-08 21:31:33 +0700

    8/8/2024

--- src/main/java/com/lcs/djbc/api/ApiApplication.java
+++ src/main/java/com/lcs/djbc/api/ApiApplication.java
@@ -18,7 +18,6 @@ import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.web.client.RestTemplate;
-
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
@@ -39,71 +38,99 @@ public class ApiApplication {
         lds();
     }
 
-        private static void lds() throws IOException{
+        private static void lds() throws IOException {
             String filename = name + sdf.format(new Date());
-                File file = new File("D:\\Project\\execute\\" + filename);
-                file.renameTo(new File("D:\\Project\\Hasil\\"+filename+".xlsx"));
-                File path = Paths.get("D:\\Project\\Hasil\\"+filename+".xlsx").toFile();
-                if (path.exists()) {
-                    FileInputStream fis = new FileInputStream(new File("D:\\Project\\Hasil\\"+filename+".xlsx"));
-                    XSSFWorkbook workbook = new XSSFWorkbook(fis);
-                    XSSFSheet sheet = workbook.getSheetAt(0);
-                    for (int i = 1; i < sheet.getPhysicalNumberOfRows() ; i++) {
-                        XSSFRow row = sheet.getRow(i);
-                        String kode = UUID.randomUUID() + "-" + sdf.format(new Date());
-                        Cell cellNOMOR_NPWP = row.getCell(0);
-                        Cell cellNAMA_PERUSAHAAN = row.getCell(1);
-                        Cell cellNOMOR_INVOICE = row.getCell(2);
-                        Cell cellTGL_INVOICE = row.getCell(3);
-                        Cell cellJENIS_VALUTA_INVOICE = row.getCell(4);
-                        Cell cellNILAI_INVOICE = row.getCell(5);
-                        //=================================================
-                        String npwp = cellNOMOR_NPWP.getStringCellValue();
-                        String nama = cellNAMA_PERUSAHAAN.getStringCellValue();
-                        String inv = cellNOMOR_INVOICE.getStringCellValue();
-                        String tgl = cellTGL_INVOICE.getStringCellValue();
-                        String jenis = cellJENIS_VALUTA_INVOICE.getStringCellValue();
-                        String nilai = cellNILAI_INVOICE.getStringCellValue();
-                        //===================================================
-                        RequestModels requestModels = new RequestModels();
-                        requestModels.setIdPerusahaan(npwp);
-                        requestModels.setNamaPerusahaan(nama);
-                        requestModels.setNomorInvoice(inv);
-                        requestModels.setTanggalInvoice(tgl);
-                        requestModels.setKodeValuta(jenis);
-                        requestModels.setNilaiTransaksi(nilai);
-                        //===================================================
-                        ObjectMapper mapper = new ObjectMapper();
-                        mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
-                        String url = "https://apis-gw.beacukai.go.id/interchange/SendDataLcs";
-                        RestTemplate restTemplate = new RestTemplate();
-                        HttpHeaders headers = new HttpHeaders();
-                        headers.setContentType(MediaType.APPLICATION_JSON);
-                        headers.set("beacukai-api-key", "ea0207a0-0d60-4f25-b77c-b5de4638f39e");
-                        JsonObject jsonObject = new JsonObject();
-                        jsonObject.addProperty("idPerusahaan", requestModels.getIdPerusahaan());
-                        log.info(requestModels.getIdPerusahaan());
-                        jsonObject.addProperty("namaPerusahaan", requestModels.getNamaPerusahaan());
-                        log.info(requestModels.getNamaPerusahaan());
-                        jsonObject.addProperty("nomorInvoice", requestModels.getNomorInvoice());
-                        jsonObject.addProperty("tanggalInvoice", requestModels.getTanggalInvoice());
-                        jsonObject.addProperty("nilaiTransaksi", requestModels.getNilaiTransaksi());
-                        jsonObject.addProperty("kodeValuta", requestModels.getKodeValuta());
-                        HttpEntity<String> request = new HttpEntity<>(jsonObject.toString(), headers);
-                        Response mo = restTemplate.postForEntity(url, request, Response.class).getBody();
-                        //===================================================
-                        FileOutputStream fout = new FileOutputStream("D:\\Project\\Sukses\\" + kode + ".txt");
-                        String out = new Gson().toJson(mo);
-                        byte er[] = out.getBytes();
-                        //converting string into byte array
-                        fout.write(er);
-                        fout.close();
-                        //=============================
-                        System.out.println("BERHASIL");
-                    }
-                } else {
-                    System.out.println("file tidak ditemukan");
+            File file = new File("D:\\Project\\execute\\" + filename);
+            file.renameTo(new File("D:\\Project\\Hasil\\" + filename + ".xlsx"));
+            File path = Paths.get("D:\\Project\\Hasil\\" + filename + ".xlsx").toFile();
+            if (path.exists()) {
+                FileInputStream fis = new FileInputStream(new File("D:\\Project\\Hasil\\" + filename + ".xlsx"));
+                XSSFWorkbook workbook = new XSSFWorkbook(fis);
+                XSSFSheet sheet = workbook.getSheetAt(0);
+                for (int i = 1; i < sheet.getPhysicalNumberOfRows(); i++) {
+                    XSSFRow row = sheet.getRow(i);
+                    String kode = UUID.randomUUID() + "-" + sdf.format(new Date());
+                    Cell cellNOMOR_NPWP = row.getCell(0);
+                    Cell cellNAMA_PERUSAHAAN = row.getCell(1);
+                    Cell cellNOMOR_INVOICE = row.getCell(2);
+                    Cell cellTGL_INVOICE = row.getCell(3);
+                    Cell cellJENIS_VALUTA_INVOICE = row.getCell(4);
+                    Cell cellNILAI_INVOICE = row.getCell(5);
+                    //=================================================
+                    String npwp = cellNOMOR_NPWP.getStringCellValue();
+                    String nama = cellNAMA_PERUSAHAAN.getStringCellValue();
+                    String inv = cellNOMOR_INVOICE.getStringCellValue();
+                    String tgl = cellTGL_INVOICE.getStringCellValue();
+                    String jenis = cellJENIS_VALUTA_INVOICE.getStringCellValue();
+                    String nilai = cellNILAI_INVOICE.getStringCellValue();
+                    //===================================================
+                    RequestModels requestModels = new RequestModels();
+                    requestModels.setIdPerusahaan(npwp);
+                    requestModels.setNamaPerusahaan(nama);
+                    requestModels.setNomorInvoice(inv);
+                    requestModels.setTanggalInvoice(tgl);
+                    requestModels.setKodeValuta(jenis);
+                    requestModels.setNilaiTransaksi(nilai);
+                    //===================================================
+                    ObjectMapper mapper = new ObjectMapper();
+                    mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+                    String url = "https://apis-gw.beacukai.go.id/interchange/SendDataLcs";
+                    RestTemplate restTemplate = new RestTemplate();
+                    HttpHeaders headers = new HttpHeaders();
+                    headers.setContentType(MediaType.APPLICATION_JSON);
+                    headers.set("beacukai-api-key", "ea0207a0-0d60-4f25-b77c-b5de4638f39e");
+                    JsonObject jsonObject = new JsonObject();
+                    jsonObject.addProperty("idPerusahaan", requestModels.getIdPerusahaan());
+                    log.info(requestModels.getIdPerusahaan());
+                    jsonObject.addProperty("namaPerusahaan", requestModels.getNamaPerusahaan());
+                    log.info(requestModels.getNamaPerusahaan());
+                    jsonObject.addProperty("nomorInvoice", requestModels.getNomorInvoice());
+                    jsonObject.addProperty("tanggalInvoice", requestModels.getTanggalInvoice());
+                    jsonObject.addProperty("nilaiTransaksi", requestModels.getNilaiTransaksi());
+                    jsonObject.addProperty("kodeValuta", requestModels.getKodeValuta());
+                    HttpEntity<String> request = new HttpEntity<>(jsonObject.toString(), headers);
+                    Response mo = restTemplate.postForEntity(url, request, Response.class).getBody();
+                    //===================================================
+                    FileOutputStream fout = new FileOutputStream("D:\\Project\\Sukses\\" + kode + ".txt");
+                    String out = new Gson().toJson(mo);
+                    byte er[] = out.getBytes();
+                    //converting string into byte array
+                    fout.write(er);
+                    fout.close();
+                    //=============================
+                    System.out.println("BERHASIL");
                 }
+            } else {
+                System.out.println("file tidak ditemukan");
+            }
+        }
 
+        public final static int BUF_SIZE = 1024; 
+
+
+            public static void createorrename(int type, File in, File out) throws Exception {
+                FileInputStream fis  = new FileInputStream(in);
+                FileOutputStream fos = new FileOutputStream(out);
+                try {
+                    byte[] buf = new byte[BUF_SIZE];
+                    int i = 0;
+                    while ((i = fis.read(buf)) != -1) {
+                        switch(type){
+                            case 0:
+                             fos.write(buf, 0, i);
+                             break;
+                            case 1:
+                             fos.renameTo(out);
+                             break;
+                        }
+                    }
+                } 
+                catch (Exception e) {
+                    throw e;
+                }
+                finally {
+                    if (fis != null) fis.close();
+                    if (fos != null) fos.close();
+                }
+            }
     }
-}
